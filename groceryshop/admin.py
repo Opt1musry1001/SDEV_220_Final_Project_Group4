2db1b606543b12a89a08edc8b5e1b4d11e2abb65
TMasters967 <tmasters3@ivytech.edu>
2023-10-12 12:23:47 -0400

    Added documentation to app -tlm

--- groceryshop/admin.py
+++ groceryshop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Food, Category, Cart, SavedCart
 
-# Register your models here.
+# Registered models.
 
 admin.site.register(Food)
 admin.site.register(Category)
